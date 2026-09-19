7c047bb98fbd7e819481a5f1eaab942d89f09fb8
reza403050 <rezashahs2020@gmail.com>
2026-09-19 16:16:30 +0330

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
- document.getElementById("image-input").addEventListener("change", function () {
- const reader = new FileReader();
- reader.onload = function () {
- document.getElementById("image-preview").style.backgroundImage = "url(" + reader.result + ")";
- // پردازش تصویر با Canvas API برای اعمال فیلترها
- const canvas = document.createElement("canvas");
- const ctx = canvas.getContext("2d");
- const img =
