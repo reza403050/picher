023ebfdf9959a8d3736cd23aa6e42ef7e5aa2910
reza403050 <rezashahs2020@gmail.com>
2026-09-19 16:45:49 +0330

    Create MainActivity.java

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.picher;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected.onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
